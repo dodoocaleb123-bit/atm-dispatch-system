06ea84285274a875ca3fe59ad19e2d8ceea50679
Caleb <dodoocaleb123@gmail.com>
2026-08-06 21:20:15 +0000

    Add PWA icons and manifest for standalone app

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        
+        {/* App Icons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        
+        {/* Theme Color for Browser Header */}
+        <meta name="theme-color" content="#9333ea" />
+      </Head>
+      <body className="bg-slate-950 text-slate-100">
         <Main />
         <NextScript />
       </body>
